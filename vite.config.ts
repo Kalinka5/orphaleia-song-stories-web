d9d084fe81cd4dc3e9ac4c8602caf2a783a6ab71
Kalinka5 <dania003d@gmail.com>
2025-05-10 15:43:51 +0300

    Update vite.config.ts file and delete js version

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,27 @@ import path from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-	server: {
-		host: "::",
-		port: 8080,
-	},
+export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: "0.0.0.0",
+		port: 8080,
+		allowedHosts: [
+			"localhost",
+			"orphaleia.com",
+			"www.orphaleia.com",
+			// Add your EC2 IP address if needed
+			// 'your-ec2-ip'
+		],
+		cors: true,
+		hmr: {
+			host: "orphaleia.com",
+			protocol: "ws",
+		},
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-}))
+})
